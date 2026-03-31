283fde0fb77d86da8ce18e0a62fa016dfe65a4c2
Mhd Sinan C <mhdsinanc313@gmail.com>
2026-03-31 17:56:55 +0530

    refactored for readability

--- Frontend/src/pages/NotFound/NotFound.jsx
+++ Frontend/src/pages/NotFound/NotFound.jsx
@@ -1,11 +1,15 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom"         // React Router link for navigation
 import './NotFound.css'
 
+// 404 Not Found page component
 export default function NotFound() {
 
     return (
         <div className="not-found">
+            {/* Error message displayed when route is not found */}
             <h1>Sorry, the page you were <br/>looking for was not found.</h1>
+
+            {/* Redirect user back to homepage */}
             <Link to={'/'}>Return to home</Link>
         </div>
     )
