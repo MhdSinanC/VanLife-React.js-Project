96dfbafe5079450f41268a44c8ff31d68c874f37
Mhd Sinan C <mhdsinanc313@gmail.com>
2026-04-06 14:28:31 +0530

    refactored for readability

--- Backend/routes/authRouter.js
+++ Backend/routes/authRouter.js
@@ -2,14 +2,23 @@ import express from 'express'
 import { loginUser, logOutUser, signupUser } from '../controllers/authController.js';
 import refreshTokenController from '../controllers/refreshTokenController.js';
 
-const authRouter = express.Router()
+const authRouter = express.Router();
 
+/**
+ * Auth Routes
+ * Base path: /api/auth
+ */
+
+// Register new user
 authRouter.post('/signup', signupUser)
 
+// Login user
 authRouter.post('/login', loginUser)
 
+// Logout user
 authRouter.post('/logout', logOutUser)
 
+// Refresh access token
 authRouter.get('/refresh', refreshTokenController) 
 
 export default authRouter;
