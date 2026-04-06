9cf79bc62b33f0b6221a02660f99ec3ca3ec9141
Mhd Sinan C <mhdsinanc313@gmail.com>
2026-04-06 15:44:16 +0530

    refactored for production ready

--- Backend/utils/generateTokens.js
+++ Backend/utils/generateTokens.js
@@ -1,19 +1,34 @@
 import jwt from 'jsonwebtoken';
 
-
+/**
+ * Generate short-lived access token
+ * Used for authenticating API requests
+ */
 export const generateToken = (user) => {
     return jwt.sign(
-        { id: user._id, email: user.email },
+        {
+            id: user._id,
+            email: user.email
+        },
         process.env.ACCESS_TOKEN_SECRET,
-        { expiresIn: '15m' }
+        {
+            expiresIn: '15m'
+        }
     )
 }
 
-
+/**
+ * Generate long-lived refresh token
+ * Used to obtain new access tokens
+ */
 export const generateRefreshToken = (user) => {
     return jwt.sign(
-        { id: user._id },
+        {
+            id: user._id
+        },
         process.env.REFRESH_TOKEN_SECRET,
-        { expiresIn: '7d' }
+        {
+            expiresIn: '7d'
+        }
     )
 }
