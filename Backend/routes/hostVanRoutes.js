a2e2f8c214839828ab8821066fb2eaff517fed0d
Mhd Sinan C <mhdsinanc313@gmail.com>
2026-04-06 15:18:04 +0530

    now using host-specific controller for getting host van

--- Backend/routes/hostVanRoutes.js
+++ Backend/routes/hostVanRoutes.js
@@ -1,20 +1,27 @@
 import express from 'express'
-import { getHostVans, postHostVan,updateVan, deleteHostVan } from '../controllers/hostVansController.js';
-import { getVanById } from '../controllers/vansController.js';
-
-
+import { getHostVans, postHostVan, updateHostVan, deleteHostVan, getHostVanById } from '../controllers/hostVansController.js';
 
 const hostVanRouter = express.Router();
 
+/**
+ * Host Van Routes
+ * Base path: /api/host
+ * Protected routes (require authentication)
+ */
 
+// Get all vans owned by host
 hostVanRouter.get('/vans', getHostVans)
 
+// Create a new van
 hostVanRouter.post('/vans', postHostVan)
 
-hostVanRouter.get('/vans/:id', getVanById)
+// Get single van (owned by host only)
+hostVanRouter.get('/vans/:id', getHostVanById)
 
-hostVanRouter.put('/vans/:id', updateVan)
+// Update van (owner only)
+hostVanRouter.put('/vans/:id', updateHostVan)
 
+// Delete van (owner only)
 hostVanRouter.delete('/vans/:id', deleteHostVan)
 
 
